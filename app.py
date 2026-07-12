487fe18f48e71b5fba5edc32520f1f549a27deea
Javier García Aguilera <106744885+JavierGarAgu@users.noreply.github.com>
2026-07-12 18:21:21 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def home():
+
+    return "Cloud Build Event Driven Demo"
+
+
+@app.get("/health")
+
+def health():
+
+    return "OK", 200
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", port=8080)
